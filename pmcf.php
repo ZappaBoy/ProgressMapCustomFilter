1412a2b00960350383f657aca4ed071bb4d5a7c0
Zappone Federico <45740621+ZappaBoy@users.noreply.github.com>
2020-02-21 22:10:59 +0100

    Update and rename main.php to pmcf.php

--- pmcf.php
+++ pmcf.php
@@ -8,161 +8,9 @@
  * Author URI: https://molise-italia.it
  */
 
-add_shortcode("search_itineraries", "main" );
-function main($attr) {
-echo '<!DOCTYPE html>
-<html>
-  <head>
-    <title>Restaurant Feedback Form</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <style>
-      html, body {
-      min-height: 100%;
-      }
-      body, div, form, input, select { 
-      padding: 0;
-      margin: 0;
-      outline: none;
-      font-family: Roboto, Arial, sans-serif;
-      font-size: 14px;
-      color: #666;
-      line-height: 22px;
-      }
-      h1, h4 {
- margin: 15px 0 4px;
-      font-weight: 400;
-      }
-      span {
-      color: red;
-      }
-      .testbox {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: inherit;
-      padding: 3px;
-      }
-      form {
-      width: 100%;
-      padding: 20px;
-      background: #fff;
-      box-shadow: 0 2px 5px #ccc; 
-      }
-      input {
-      width: calc(100% - 10px);
-      padding: 5px;
-      border: 1px solid #ccc;
-      border-radius: 3px;
-      vertical-align: middle;
-      }
-      input:hover, textarea:hover, select:hover {
-      outline: none;
-      border: 1px solid #095484;
-      }
-      .name input {
-      margin-bottom: 10px;
-      }
-      select {
-      padding: 7px 0;
-      border-radius: 3px;
-      border: 1px solid #ccc;
-      background: #e6eef7;
-      }
-      option {
-      background: #fff;
-      }
-      select, table {
-      width: 100%;
-      }
-      .day-visited, .time-visited {
-      position: relative;
-      }
-      .day-visited input, .time-visited input {
-      width: calc(100% - 12px);
-      background: #e6eef7;
-      }
-      input[type="date"]::-webkit-inner-spin-button {
-      display: none;
- }
-      input[type="time"]::-webkit-inner-spin-button {
-      margin: 2px 22px 0 0;
-      }
-      .day-visited i, .time-visited i, input[type="date"]::-webkit-calendar-picker-indicator {
-      position: absolute;
-      top: 8px;
-      font-size: 20px;
-      }
-      .day-visited i, .time-visited i {
-      right: 5px;
-      z-index: 1;
-      color: #a9a9a9;
-      }
-      [type="date"]::-webkit-calendar-picker-indicator {
-      right: 0;
-      z-index: 2;
-      opacity: 0;
-      }
-      .question-answer label {
-      display: block;
-      padding: 0 20px 10px 0;
-      }
-      .question-answer input {
-      width: auto;
-      margin-top: -2px;
-      }
-      th, td {
-      width: 18%;
-      padding: 15px 0;
-      border-bottom: 1px solid #ccc;
-      text-align: center;
-      vertical-align: unset;
-      line-height: 18px;
-      font-weight: 400;
-      word-break: break-all;
-      }
-      .first-col {
-      width: 25%;
-      text-align: left;
-      }
-      textarea {
-      width: calc(100% - 6px);
-      }
-      .btn-block {
-      margin-top: 20px;
-      text-align: center;
-      }
-      button {
-      width: 150px;
-      padding: 10px;
-      border: none;
-      -webkit-border-radius: 5px; 
--moz-border-radius: 5px; 
-      border-radius: 5px; 
-      background-color: #095484;
-      font-size: 16px;
-      color: #fff;
-      cursor: pointer;
-      }
-      button:hover {
-      background-color: #0666a3;
-      }
-      @media (min-width: 568px) {
-      .name {
-      display: flex;
-      justify-content: space-between;
-      }
-      .name input {
-      width: 47%;
-      margin-bottom: 0;
-      }
-      th, td {
-      word-break: keep-all;
-      }
-      }
-    </style>
-  </head>
-  <body>
+add_shortcode("pmcf_search_itineraries", "pmcf_show_form" );
+function pmcf_show_form($attr) {
+echo '
     <div class="testbox">
       <form action="/">
         <h1>Restaurant Feedback Form</h1>
@@ -260,9 +108,7 @@ echo '<!DOCTYPE html>
           <button type="submit" href="/">Send Feedback</button>
         </div>
       </form>
-    </div>
-  </body>
-</html>';
+    </div> ';
 
 }
 
