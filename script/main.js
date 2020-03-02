e7c1343492c0b39b67d7c9991517ef4528aa4f8d
RedHitMark <russodivito.marco@gmail.com>
2020-03-02 22:22:07 +0100

    new jquery form management

--- script/main.js
+++ script/main.js
@@ -1,6 +1,55 @@
 jQuery.noConflict()(function($){
     "use strict";
     $(document).ready(function() {
+        let answerObj = [];
+
+        $(".question-1").show();
+        $(".question-2").hide();
+        $(".question-3").hide();
+        $(".question-4").hide();
+
+        //Question 1
+        $('.question-1 .answer1').click(function () {
+            $(".question-1").hide("slow");
+            $(".question-2").show();
+            answerObj[0] = 1;
+        });
+        $('.question-1 .answer2').click(function () {
+            $(".question-1").hide();
+            $(".question-2").show();
+            answerObj[0] = 2;
+        });
+        $('.question-1 .answer3').click(function () {
+            $(".question-1").hide();
+            $(".question-1").show();
+            answerObj[0] = 3;
+        });
+
+        //Question 2
+        $('.question-2 .answer1').click(function () {
+            $(".question-1").hide();
+            $(".question-2").hide();
+            $(".question-3").show();
+        });
+        $('.question-2 .answer2').click(function () {
+            $(".question-1").hide();
+            $(".question-2").hide();
+            $(".question-3").show();
+        });
+        $('.question-2 .answer3').click(function () {
+            $(".question-1").hide();
+            $(".question-2").hide();
+            $(".question-3").show();
+        });
+        $('.question-2 .previews-question').click(function () {
+            $(".question-1").show();
+            $(".question-2").hide();
+            $(".question-3").hide();
+        });
+
+
+
+
         $(".locations").hide();
         $(".other-questions").hide();
 
