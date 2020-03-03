a2a6219dc1ccf79148ac1d3001cb665f41c564ed
RedHitMark <russodivito.marco@gmail.com>
2020-03-03 12:59:15 +0100

    new form

--- script/main.js
+++ script/main.js
@@ -5,21 +5,27 @@ jQuery.noConflict()(function($){
         let answerObj = [];
 
 
-
-        for (let questionIndex = 1; questionIndex <= 7; questionIndex++) {
+        for (let questionIndex = 1; questionIndex <= 8; questionIndex++) {
             $('.question-' + questionIndex).hide();
             for (let answerIndex = 1; answerIndex <= 3; ++answerIndex) {
                 $('.question-' + questionIndex + ' .answer' + answerIndex).click(function () {
-                    //hide this question container and show the next one
-                    $('.question-' + questionIndex).hide('slow');
+                    if(questionIndex === 8) {
+                        console.log(answerObj);
+                    } else if (questionIndex === 7 && answerIndex === 2) {
+                        console.log(answerObj);
+                        //TODO handle form
+                    } else {
+                        //hide this question container and show the next one
+                        $('.question-' + questionIndex).hide('slow');
 
-                    $('.question-' + (questionIndex +1)).show();
+                        $('.question-' + (questionIndex +1)).show();
 
-                    answerObj[questionIndex] = answerIndex;
+                        answerObj[questionIndex] = answerIndex;
+                    }
                 });
 
                 $('.question-' + questionIndex + ' .previews-question').click(function () {
-                    //hide this question container and show the next one
+                    //hide this question container and show the previous one
                     $('.question-' + questionIndex).hide('slow');
 
                     $('.question-' + (questionIndex -1)).show();
